2a18af89b839526996fd368ee914ea171e8e3942
MarkWeng <bskin0330@gmail.com>
2018-02-14 20:41:44 +0800

    fixed category restaurant sort

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @restaurants = @category.restaurants.page(params[:page]).per(9)
+    @restaurants = @category.restaurants.order(created_at: :desc).page(params[:page]).per(9)
   end
 end
